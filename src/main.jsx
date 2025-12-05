e2f6b3d17b903ad9a3bd01b592a908b0d79b3780
Dipesh8886 <dipeshrawat0408@gmail.com>
2025-12-05 22:58:35 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import AuthProvider from "./auth";
-import "./index.css";
+import "./Index.css";
 import "./app.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
