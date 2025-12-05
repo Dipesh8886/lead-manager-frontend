8361c6c83c5e0c771974e53c22c9e0e8c8402f21
Dipesh8886 <dipeshrawat0408@gmail.com>
2025-12-05 23:00:56 +0530

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import AuthProvider from "./auth";
 import "./Index.css";
-import "./app.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
